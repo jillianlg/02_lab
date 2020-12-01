e301a79475c7a319c57628e3f381a4e17783e328
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-01 14:44:48 -0800

    getName passed with bracket notation

--- getName/getName.js
+++ getName/getName.js
@@ -1,5 +1,12 @@
-const getName = () => {
-    return "spot"
+const spot = {
+    name: 'spot',
+    age: 5,
+    weight: '20 lbs'
+};
+
+const getName = (spot) => {
+    const name = spot['name']
+        return name;
 };
 
 module.exports = {
--- getName/getName.test.js
+++ getName/getName.test.js
@@ -1,8 +1,14 @@
 const { getName } = require('./getName');
 
 describe('getName', () => {
+    const spot = {
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+    };
+
     it('return the name from the array', () => {
-        const name = getName('spot');
+        const name = getName(spot);
 
         expect(name)
             .toEqual('spot')
