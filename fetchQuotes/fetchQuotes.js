766e3b6591cb7cfc2779947072bf83ebe9009651
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-01 20:16:45 -0800

    fetchQuote passed with destructured code

--- fetchQuotes/fetchQuotes.js
+++ fetchQuotes/fetchQuotes.js
@@ -1,7 +1,18 @@
 const request = require('superagent');
 
-const fetchQuotes = () => {
-    
+const fetchQuotes = async () => {
+    try {
+        const response = await request.get(
+        "http://futuramaapi.herokuapp.com/api/quotes/"
+        );
+
+        return response.body
+            .map(({ character: name }) => name)
+            .map(({ quote: text }) => text)
+            .map(({ image }) => image);
+    } catch (err) {
+        console.log("Error!: ", err);
+    }
 };
 
 module.exports = {
--- fetchQuotes/fetchQuotes.test.js
+++ fetchQuotes/fetchQuotes.test.js
@@ -9,6 +9,7 @@ describe('fetchQuotes', () => {
             image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
         }
 
+
         expect(quote).toEqual({
                 name: 'Bender',
                 text: 'I\'m a fraud. A poor, lazy, sexy fraud.',
