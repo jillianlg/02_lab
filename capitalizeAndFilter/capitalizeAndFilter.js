5dec774ce53bea27bae71966bc783aac4e545d24
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-01 16:49:11 -0800

    capitalizaAndFilter add and log capitalize item with for loop

--- capitalizeAndFilter/capitalizeAndFilter.js
+++ capitalizeAndFilter/capitalizeAndFilter.js
@@ -1,5 +1,13 @@
-const capitalizeAndFilter = () => {
-    return ['SPOT', 'ROVER', 'BINGO']
+const capitalizeAndFilter = (array) => {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        const item = array[i];
+        console.log(item);
+        const capitalItem = item.toUpperCase();
+        console.log(capitalItem);
+    }
+
+    return newArray
 };
 
 module.exports = {
